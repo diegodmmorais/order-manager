fd25237509312e1c2c6fb2dde4cc793eee91909a
Diego Morais <diego.moras@labsit.com>
2022-05-12 15:34:54 -0300

    implementando teste unitario para ordergateway

---
+++ gateway/order/mock_order_repository.go
@@ -0,0 +1,13 @@
+package order
+
+import "github.com/stretchr/testify/mock"
+
+type MockOrderRepository struct {
+	mock.Mock
+}
+
+func (mock *MockOrderRepository) Save(orderMapper OrderInputMapper) (string, error) {
+	args := mock.Called()
+	result := args.Get(0)
+	return result.(string), args.Error(1)
+}
